1f813ea4c0164c34a3ce76b8bf5ca2df6071e746
Satvik Sharma <sharmasatvik58@gmail.com>
2025-06-16 18:52:13 -0700

    Deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import Home from './pages/home/Home.jsx';
-import Login from './pages/login/Login.jsx';
-import Signup from './pages/signup/Signup.jsx'
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
 
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
-const App = () => {
-  return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Signup /> */}
-      {/* <Login /> */}
-      <Home />
-    </div>
-  )
+function App() {
+	const { authUser } = useAuthContext();
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
-export default App
+export default App;
